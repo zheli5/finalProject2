06f32ff47b00ad7e8bfb029a46b1c4bd48b40162
qianfut2 <qianfut2@illinois.edu>
2018-12-07 19:19:19 -0600

    import volley

--- app/src/main/java/com/example/daniel/stocktracker/GoogleInc.java
+++ app/src/main/java/com/example/daniel/stocktracker/GoogleInc.java
@@ -1,7 +1,14 @@
 package com.example.daniel.stocktracker;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import org.json.JSONObject;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 
 public class GoogleInc extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class GoogleInc extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_inc);
+    }
+
+    JsonObjectRequest jsonObjectRequest;
+
+    {
+        jsonObjectRequest = new JsonObjectRequest(
+                DownloadManager.Request.Method.GET,
+                "https://api.iextrading.com/1.0/stock/AAPL/batch\n",
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(final JSONObject response) {
+                        Log.d(TAG, response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(final VolleyError error) {
+                Log.w(TAG, error.toString());
+            }
+        });
     }
 }
